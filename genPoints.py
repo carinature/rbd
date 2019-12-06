6749b0f1c61970d1161e0f41d6404cd3a4166470
Karina Ture <karina@campus.technion.ac.il>
2019-12-07 01:42:34 +0200

    gen_encrypted_points_from_file working correctly, create list of points of Binaries (of floats)

--- genPoints.py
+++ genPoints.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 from random import *
-
+from auxClasses import *
 dim = 3  # #dimansions
 size = 100
 file = "/home/rbd/PycharmProjects/dan/points"
 
 
-def rand(range_lim=10, round_lim=2):
-    return round(range_lim * random(), round_lim)
+def rand(bottom_lim=0, range_lim=10, round_lim=2):
+    return round(range_lim * random() + bottom_lim, round_lim)
 
 
 def generate_points(n=size, d=dim):
@@ -36,9 +36,21 @@ def get_points(n=size, d=dim):
     return get_points_from_file()
 
 
+def gen_encrypted_points_from_file():
+    with open(file) as f:
+        points = [Point(*map(Binary, map(float, i.split(' ')))) for i in f]  # todo encapsulate in Binary
+        print(points)
+    return points
+
+
+# def gen_encrypted_points():
+
+
 if __name__ == '__main__':
     # create_points_file()
     # generate_points()
-    print('get_points')
-    print(get_points())
+    # print('get_points')
+    # print(get_points())
+    print('gen_encrypted_points_from_file')
+    print(gen_encrypted_points_from_file())
 
